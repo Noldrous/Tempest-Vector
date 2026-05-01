f8ec9ed3214211c7f744c7360c827add00092652
Wency Villegas <villegaswency20@gmail.com>
2026-05-01 15:33:38 +0800

    added visuals for rocket explosion

---
+++ particle.py
@@ -0,0 +1,16 @@
+from settings import *
+
+class Particle:
+    def __init__(self, pos, velocity):
+        self.pos = pygame.Vector2(pos)
+        self.velocity = velocity
+        self.life = 5
+
+    def update(self):
+        self.pos += self.velocity
+        self.life -= 1
+
+    def draw(self, screen):
+        if self.life > 0:
+            pygame.draw.rect(screen, (255, 0, 0), (int(self.pos.x), int(self.pos.y), 8 , 8))
+            pygame.draw.rect(screen, (255, 150, 50), (int(self.pos.x)+2, int(self.pos.y)+2, 4, 4))
